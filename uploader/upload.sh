b8abeb93adaa315228b81c52670fff5e02fd2f88
syspro86 <syspro86@gmail.com>
2022-10-19 00:38:44 +0900

    pushd를 cd로 변경

--- uploader/upload.sh
+++ uploader/upload.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-pushd $(dirname $0)
+cd $(dirname $0)
 source ./query_var.env
 
 ./get_cpu.sh
 ./get_memory.sh
 
-pushd ../data/
+cd ../data/
 git pull
 git add ${DT_YEAR}/${DT_MONTH}/${DT_DAY}
 echo ${DT_YEAR}-${DT_MONTH}-${DT_DAY} | git commit -F-
